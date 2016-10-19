49189fc0fc1e843da5f8204e419274367ea1b563
Cappadocia <530524060@qq.com>
2016-10-19 15:40:07 +0800

    更新generic.py和linux.py内容

--- d08/monitor_demo/server_demo/conf/services/linux.py
+++ d08/monitor_demo/server_demo/conf/services/linux.py
@@ -1,2 +1,55 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+import generic  
+from data_process import avg,hit,last
+
+class cpu(generic.BaseService):
+    def __init__(self):
+        super(cpu,self).__init__()
+        self.name = 'linux_cpu'
+        self.interval = 30
+        self.plugin_name = 'get_cpu_status'
+        self.truggers = {
+                'idle':{'func':avg,
+                        'minutes':15,
+                        'operator':'lt',
+                        #'threshold':0,
+                        'warning':20,
+                        'critical':5,
+                        'data_type':'percentage'
+                        },
+                'iowait':{'func':hit,
+                        'minutes':10,
+                        'operator':'gt',
+                        'threshold':3,
+                        'warning':50,
+                        'critical':80,
+                        'data_type':'percentage'
+                        },
+                         }
+        
+class memory(generic.BaseService):
+    def __init__(self):
+        super(memory,self).__init__()
+        self.name = 'linux_memory'
+        self.interval = 60
+        self.plugin_name = 'get_memory_info'
+        self.truggers = {
+                'idle':{'func':avg,
+                        'minutes':15,
+                        'operator':'gt',
+                        #'threshold':0,
+                        'warning':80,
+                        'critical':90,
+                        'data_type':'percentage'
+                        }
+                         }
+        
+        
+        
+if __name__ == '__main__':
+    c = cpu()
+    print(c.name,c.interval,c.plugin_name)
+    
+    
