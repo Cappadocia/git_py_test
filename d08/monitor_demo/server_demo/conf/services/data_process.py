7fd4ede8a38065d426ed088434bc24e75d16307f
Cappadocia <530524060@qq.com>
2016-10-19 21:42:24 +0800

    新增data_process.py代码

--- d08/monitor_demo/server_demo/conf/services/data_process.py
+++ d08/monitor_demo/server_demo/conf/services/data_process.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+
+def avg(mins,operator,data):
+    print('run-----avg------',mins,operator,data)
+    
+def last(mins,operator,data):
+    print('run-----last------',mins,operator,data)
+    
+def hit(mins,operator,data):
+    print('run-----hit------',mins,operator,data)
