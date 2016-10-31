fc5c6d7a0d86becb69fde654ff143e908d3caf80
Cappadocia <530524060@qq.com>
2016-10-31 17:34:19 +0800

    新增jQuery练习 搜索框练习 跑马灯练习

--- d10/js/ccx.js
+++ d10/js/ccx.js
@@ -26,6 +26,47 @@ function Bar(){
 */
 
 //自执行函数
+/*
 (function(name){
 	console.log(name);
 })('ccx')
+*/
+
+/*
+//声明数组
+var arry=[1,2,3,4]
+
+//var arry2 = Array(1,2,3,4,5)
+
+arry.push('ccx');
+console.log(arry);
+arry.unshift('yound');
+console.log(arry);
+arry.splice(1,0,'boy');
+console.log(arry);
+arry.splice(100,0,'pk');
+console.log(arry);
+*/
+
+
+
+/*
+var array = [11,22,33,44,55]
+var dict = {'name':'ccx','age':18}
+for(var item in array){
+	console.log(item)
+}
+for(var item in dict){
+	console.log(item)
+}
+
+
+for(var i=0;i<array.length;i++){
+	console.log(dict[i])
+}
+
+for(var i=0;i<10;i++){
+	console.log(dict[i])
+}
+*/
+
